4f90be0fbd81fcaab1727bb153c7f4a402665e6e
DPhngNam <22520908@gm.uit.edu.vn>
2025-02-14 23:13:36 +0700

    Finished the fifth semester

---
+++ SE114/ChatApp/app/src/main/java/com/example/chatapp_testing/activities/signIn.java
@@ -0,0 +1,16 @@
+package com.example.chatapp_testing.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapp_testing.R;
+
+public class signIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
---
+++ SE114/ChatApp/app/src/main/java/com/example/chatapp_testing/activities/signup.java
@@ -0,0 +1,16 @@
+package com.example.chatapp_testing.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatapp_testing.R;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
